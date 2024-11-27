c6f6ce2b3f1784dd80f76f9391ecd0ba1986afdd
Doug Johnson <doug@asknice.com>
2024-11-27 17:05:10 +0530

    put refund information into form

--- resources/views/Public/ViewEvent/Partials/EventCreateOrderSection.blade.php
+++ resources/views/Public/ViewEvent/Partials/EventCreateOrderSection.blade.php
@@ -20,35 +20,36 @@
                 <div class="panel-body pt0">
                     <table class="table mb0 table-condensed">
                         @foreach($tickets as $ticket)
-                        <tr>
-                            <td class="pl0">{{{$ticket['ticket']['title']}}} X <b>{{$ticket['qty']}}</b></td>
-                            <td style="text-align: right;">
-                                @isFree($ticket['full_price'])
+                            <tr>
+                                <td class="pl0">{{{$ticket['ticket']['title']}}} X <b>{{$ticket['qty']}}</b></td>
+                                <td style="text-align: right;">
+                                    @isFree($ticket['full_price'])
                                     @lang("Public_ViewEvent.free")
-                                @else
-                                {{ money($ticket['full_price'], $event->currency) }}
-                                @endif
-                            </td>
-                        </tr>
+                                    @else
+                                        {{ money($ticket['full_price'], $event->currency) }}
+                                    @endif
+                                </td>
+                            </tr>
                         @endforeach
                     </table>
                 </div>
                 @if($order_total > 0)
-                <div class="panel-footer">
-                    <h5>
-                        @lang("Public_ViewEvent.total"): <span style="float: right;"><b>{{ $orderService->getOrderTotalWithBookingFee(true) }}</b></span>
-                    </h5>
-                    @if($event->organiser->charge_tax)
-                    <h5>
-                        {{ $event->organiser->tax_name }} ({{ $event->organiser->tax_value }}%):
-                        <span style="float: right;"><b>{{ $orderService->getTaxAmount(true) }}</b></span>
-                    </h5>
-                    <h5>
-                        <strong>@lang("Public_ViewEvent.grand_total")</strong>
-                        <span style="float: right;"><b>{{  $orderService->getGrandTotal(true) }}</b></span>
-                    </h5>
-                    @endif
-                </div>
+                    <div class="panel-footer">
+                        <h5>
+                            @lang("Public_ViewEvent.total"): <span
+                                    style="float: right;"><b>{{ $orderService->getOrderTotalWithBookingFee(true) }}</b></span>
+                        </h5>
+                        @if($event->organiser->charge_tax)
+                            <h5>
+                                {{ $event->organiser->tax_name }} ({{ $event->organiser->tax_value }}%):
+                                <span style="float: right;"><b>{{ $orderService->getTaxAmount(true) }}</b></span>
+                            </h5>
+                            <h5>
+                                <strong>@lang("Public_ViewEvent.grand_total")</strong>
+                                <span style="float: right;"><b>{{  $orderService->getGrandTotal(true) }}</b></span>
+                            </h5>
+                        @endif
+                    </div>
                 @endif
 
             </div>
@@ -88,18 +89,22 @@
                         </div>
                     </div>
                 </div>
-                <div class="row"><div class="col-md-12">&nbsp;</div></div>
+                <div class="row">
+                    <div class="col-md-12">&nbsp;</div>
+                </div>
                 <div class="row">
                     <div class="col-md-12">
-{{--                        <div class="form-group">--}}
-{{--                            <div class="custom-checkbox">--}}
-{{--                                {!! Form::checkbox('is_business', 1, null, ['data-toggle' => 'toggle', 'id' => 'is_business']) !!}--}}
-{{--                                {!! Form::label('is_business', trans("Public_ViewEvent.is_business"), ['class' => 'control-label']) !!}--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
+                        {{--                        <div class="form-group">--}}
+                        {{--                            <div class="custom-checkbox">--}}
+                        {{--                                {!! Form::checkbox('is_business', 1, null, ['data-toggle' => 'toggle', 'id' => 'is_business']) !!}--}}
+                        {{--                                {!! Form::label('is_business', trans("Public_ViewEvent.is_business"), ['class' => 'control-label']) !!}--}}
+                        {{--                            </div>--}}
+                        {{--                        </div>--}}
                     </div>
                 </div>
-                <div class="row"><div class="col-md-12">&nbsp;</div></div>
+                <div class="row">
+                    <div class="col-md-12">&nbsp;</div>
+                </div>
                 <div class="row hidden" id="business_details">
                     <div class="col-md-12">
                         <div class="form-group">
@@ -154,7 +159,9 @@
                         </div>
                     </div>
                 </div>
-                <div class="row"><div class="col-md-12">&nbsp;</div></div>
+                <div class="row">
+                    <div class="col-md-12">&nbsp;</div>
+                </div>
                 <div class="p20 pl0">
                     <a href="javascript:void(0);" class="btn btn-primary btn-xs" id="mirror_buyer_info">
                         @lang("Public_ViewEvent.copy_buyer")
@@ -163,67 +170,96 @@
 
                 <div class="row">
                     <div class="col-md-12">
-                        <div class="ticket_holders_details" >
+                        <div class="ticket_holders_details">
                             <h3>@lang("Public_ViewEvent.ticket_holder_information")</h3>
                             <?php
-                                $total_attendee_increment = 0;
+                            $total_attendee_increment = 0;
                             ?>
                             @foreach($tickets as $ticket)
                                 @for($i=0; $i<=$ticket['qty']-1; $i++)
-                                <div class="panel panel-primary">
+                                    <div class="panel panel-primary">
 
-                                    <div class="panel-heading">
-                                        <h3 class="panel-title">
-                                            <b>{{$ticket['ticket']['title']}}</b>: @lang("Public_ViewEvent.ticket_holder_n", ["n"=>$i+1])
-                                        </h3>
-                                    </div>
-                                    <div class="panel-body">
-                                        <div class="row">
-                                            <div class="col-md-6">
-                                                <div class="form-group">
-                                                    {!! Form::label("ticket_holder_first_name[{$i}][{$ticket['ticket']['id']}]", trans("Public_ViewEvent.first_name")) !!}
-                                                    {!! Form::text("ticket_holder_first_name[{$i}][{$ticket['ticket']['id']}]", null, ['required' => 'required', 'class' => "ticket_holder_first_name.$i.{$ticket['ticket']['id']} ticket_holder_first_name form-control"]) !!}
+                                        <div class="panel-heading">
+                                            <h3 class="panel-title">
+                                                <b>{{$ticket['ticket']['title']}}</b>: @lang("Public_ViewEvent.ticket_holder_n", ["n"=>$i+1])
+                                            </h3>
+                                        </div>
+                                        <div class="panel-body">
+                                            <div class="row">
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                        {!! Form::label("ticket_holder_first_name[{$i}][{$ticket['ticket']['id']}]", trans("Public_ViewEvent.first_name")) !!}
+                                                        {!! Form::text("ticket_holder_first_name[{$i}][{$ticket['ticket']['id']}]", null, ['required' => 'required', 'class' => "ticket_holder_first_name.$i.{$ticket['ticket']['id']} ticket_holder_first_name form-control"]) !!}
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                        {!! Form::label("ticket_holder_last_name[{$i}][{$ticket['ticket']['id']}]", trans("Public_ViewEvent.last_name")) !!}
+                                                        {!! Form::text("ticket_holder_last_name[{$i}][{$ticket['ticket']['id']}]", null, ['required' => 'required', 'class' => "ticket_holder_last_name.$i.{$ticket['ticket']['id']} ticket_holder_last_name form-control"]) !!}
+                                                    </div>
                                                 </div>
                                             </div>
-                                            <div class="col-md-6">
-                                                <div class="form-group">
-                                                    {!! Form::label("ticket_holder_last_name[{$i}][{$ticket['ticket']['id']}]", trans("Public_ViewEvent.last_name")) !!}
-                                                    {!! Form::text("ticket_holder_last_name[{$i}][{$ticket['ticket']['id']}]", null, ['required' => 'required', 'class' => "ticket_holder_last_name.$i.{$ticket['ticket']['id']} ticket_holder_last_name form-control"]) !!}
+                                            <div class="row">
+                                                <div class="col-md-12">
+                                                    <div class="form-group">
+                                                        {!! Form::label("ticket_holder_email[{$i}][{$ticket['ticket']['id']}]", trans("Public_ViewEvent.email_address")) !!}
+                                                        {!! Form::text("ticket_holder_email[{$i}][{$ticket['ticket']['id']}]", null, ['required' => 'required', 'class' => "ticket_holder_email.$i.{$ticket['ticket']['id']} ticket_holder_email form-control"]) !!}
+                                                    </div>
                                                 </div>
+                                                @include('Public.ViewEvent.Partials.AttendeeQuestions', ['ticket' => $ticket['ticket'],'attendee_number' => $total_attendee_increment++])
+
                                             </div>
                                         </div>
-                                        <div class="row">
-                                            <div class="col-md-12">
-                                                <div class="form-group">
-                                                    {!! Form::label("ticket_holder_email[{$i}][{$ticket['ticket']['id']}]", trans("Public_ViewEvent.email_address")) !!}
-                                                    {!! Form::text("ticket_holder_email[{$i}][{$ticket['ticket']['id']}]", null, ['required' => 'required', 'class' => "ticket_holder_email.$i.{$ticket['ticket']['id']} ticket_holder_email form-control"]) !!}
-                                                </div>
-                                            </div>
-                                            @include('Public.ViewEvent.Partials.AttendeeQuestions', ['ticket' => $ticket['ticket'],'attendee_number' => $total_attendee_increment++])
-
-                                        </div>
                                     </div>
-                                </div>
                                 @endfor
                             @endforeach
                         </div>
                     </div>
                 </div>
-
                 @if($event->pre_order_display_message)
-                <div class="well well-small">
-                    {!! nl2br(e($event->pre_order_display_message)) !!}
-                </div>
+                    <div class="well well-small">
+                        {!! nl2br(e($event->pre_order_display_message)) !!}
+                    </div>
+                    <div class="well well-small">
+                        <h3><strong>RWA2025 Conference Registration Refund Policy</strong></h3>
+                        <p><strong>1. Refund Deadlines and Fees:</strong><br>
+                            o Refund requests received between <strong>December 3rd and February 14th</strong> will be
+                            processed with a <strong>$100 processing fee</strong>.<br>
+                            o Requests received between <strong>February 15th and April 2nd</strong> will be processed
+                            with a <strong>$150 processing fee.</strong><br>
+                            o Requests received between <strong>April 3rd and June 1st</strong> will be processed with a
+                            <strong>$200 processing fee</strong>.<br>
+                            o <strong>No refunds</strong> will be granted after <strong>June 1st</strong>.</p><br>
+                        <p><strong>2. Payment Plan Registrations:</strong><br>
+                            o The <strong>first payment is nonrefundable</strong>.</p>
+                        <p><strong>3. Exceptional Circumstances:</strong><br>
+                            Refunds may be issued without a cancellation fee if documentation is provided showing the
+                            registrant could not attend due to:<br>
+                            o Serious illness or death of the registrant or an immediate family member.<br>
+                            o Inability to travel due to natural disasters, war, government regulations, or acts of
+                            terrorism.</p>
+                        <p><strong>4. Comped Registration Policy:</strong>
+                            If a volunteer or committee member with a comped registration cannot attend due to serious
+                            illness, death of an immediate family member, or travel restrictions (natural disasters, war,
+                            etc.):<br>
+                            o The comped registration may be applied to the following year upon submission of written
+                            proof.<br>
+                            o The volunteer or committee member must pay the applicable processing fee and any increase
+                            in registration costs.<br>
+                            o The comped registration is <strong>non-transferable</strong> to another individual.</p>
+
+                    </div>
                 @endif
 
-               {!! Form::hidden('is_embedded', $is_embedded) !!}
-               {!! Form::submit(trans("Public_ViewEvent.checkout_order"), ['class' => 'btn btn-lg btn-success card-submit', 'style' => 'width:100%;']) !!}
-               {!! Form::close() !!}
+
+                {!! Form::hidden('is_embedded', $is_embedded) !!}
+                {!! Form::submit(trans("Public_ViewEvent.checkout_order"), ['class' => 'btn btn-lg btn-success card-submit', 'style' => 'width:100%;']) !!}
+                {!! Form::close() !!}
 
             </div>
         </div>
     </div>
-    <img src="https://cdn.attendize.com/lg.png" />
+    <img src="https://cdn.attendize.com/lg.png"/>
 </section>
 @if(session()->get('message'))
     <script>showMessage('{{session()->get('message')}}');</script>
